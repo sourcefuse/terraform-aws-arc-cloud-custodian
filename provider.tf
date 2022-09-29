2e20bc7b505b6c97c2e0f58b154b75118ff9d141
mayank0202 <mayank.sharma@sourcefuse.com>
2022-09-30 02:28:52 +0530

    added types in variables and added provider of local

---
+++ provider.tf
@@ -0,0 +1,40 @@
+###########################################
+## defaults / versions / providers
+###########################################
+terraform {
+  required_version = ">= 1.0.8"
+
+#   backend "s3" {
+#     region         = "us-east-1"
+#     key            = "terraform-aws-cloud-custodian/terraform.tfstate"
+#     bucket         = "sf-ref-arch-terraform-state-dev"
+#     dynamodb_table = "sf_ref_arch_terraform_state_dev"
+#     encrypt        = true
+#   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+
+     local = {
+      source = "hashicorp/local"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
